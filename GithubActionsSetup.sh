162637a8bf39ad53f287657c1b350be398965615
Name <me@email.com>
2020-09-24 18:50:12 -0500

    Commit6

--- GithubActionsSetup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-git clone $1
-mkdir $2/.github
-mkdir $2/.github/workflows
-touch $2/.github/workflows/$3.yaml
-git --git-dir=$2 add *
-git --git-dir=$2 commit -m "Commit"
-git --git-dir=$2 push -u origin master
-rm -rf $2
